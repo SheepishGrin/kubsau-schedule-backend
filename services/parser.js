41363789017467160afdb3f81ffd188607d14b04
SheepishGrin <134944584+SheepishGrin@users.noreply.github.com>
2026-03-30 20:38:34 +0300

    Update parser.js

--- services/parser.js
+++ services/parser.js
@@ -74,7 +74,7 @@ function parseSchedule(html, groupName) {
             // В исходном HTML у лекций класс "lection", у семинаров – "lection yes"
             const lectionClasses = $lectionCell.attr('class') || '';
             const isSeminar = lectionClasses.includes('yes');
-            const type = isSeminar ? 'seminar' : 'lecture';
+            const type = ! isSeminar ? 'seminar' : 'lecture';
 
             // --- Предмет и преподаватели ---
             // Копируем ячейку, удаляем вложенные .diss-info, чтобы получить чистое название предмета
